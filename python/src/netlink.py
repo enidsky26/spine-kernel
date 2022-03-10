572221f3adaffa1f32d4788242933f8f0facf4c8
Xudong Liao <stephenxudong@gmail.com>
2022-03-10 21:25:15 +0800

    init python part

---
+++ python/src/netlink.py
@@ -0,0 +1,53 @@
+import socket
+import os
+import sys
+import struct
+
+SOL_NETLINK = 270
+NETLINK_ADD_MEMBERSHIP = 1
+
+NETLINK_GROUP = 22
+
+MAX_PAYLOAD_LEN = 1024
+
+class Netlink(object):
+    def __init__(self, protocol=socket.NETLINK_USERSOCK, nl_group=0):
+        self.sock = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, protocol)
+        assert (self.sock.fileno() > 0, "Invalid socket fileno")
+        # bind <port, group>
+        # set port to 0 to let kernel assign port number (usually the pid of current process)
+        # The default value for this field (group)is zero which means that no multicasts will be received 
+        self.sock.bind((0, nl_group))
+        self.port = self.sock.getsockname()[0]
+        self.nl_header_len = 16
+
+
+    def add_mc_group(self, group=NETLINK_GROUP):
+        self.sock.setsockopt(SOL_NETLINK, NETLINK_ADD_MEMBERSHIP, group)
+
+    def send_msg(self, message: bytes, des=0):
+        total_len = len(message) + self.nl_header_len
+        if  total_len > MAX_PAYLOAD_LEN:
+            sys.stderr.write("netlink message is too long.")
+            return -1
+        buf = self.write_header(total_len) + message
+        # des 0 means sending to kernel
+        # we don't need to specify nl_group here
+        return self.sock.sendto(message, (des, 0))
+
+    def write_header(self, len=MAX_PAYLOAD_LEN):
+        """ prepare netlink header (16 bytes)
+        0               1               2               3
+        0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                          Length                             |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |            Type              |           Flags              |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                      Sequence Number                        |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                      Process ID (PID)                       |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        """
+        nl_header = struct.pack("=IHHII", len, 0, 0, 0, self.port)
+        return nl_header
